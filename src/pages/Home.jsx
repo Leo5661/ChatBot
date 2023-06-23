8ba05246e178ea19da9426dc041309fe56bf2f3c
Leo5661 <singhra66@gmail.com>
2023-06-23 05:54:44 +0530

    Added: Link to ChatBot Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div className="home flex h-full flex-col items-center justify-around bg-white">
       <div className="title w-full text-center text-xl tracking-wider text-neutral-900">
         Enter into student <br /> info system
       </div>
-      <div className="enroll_button flex items-center justify-center rounded bg-slate-600 px-4 py-2 text-white shadow-xl">
+      <Link
+        className="enroll_button flex items-center justify-center rounded bg-slate-600 px-4 py-2 text-white shadow-xl"
+        to={`/chatbot`}
+      >
         Enroll Now &rarr;
-      </div>
+      </Link>
     </div>
   );
 }
