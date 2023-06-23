06f8f8ebc8c92da212a8fef27a3ecdb767113972
Leo5661 <singhra66@gmail.com>
2023-06-24 00:43:18 +0530

    Fix: fixed store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { studentSlice } from "./slices/student";
+import studentSlice from "./slices/student";
 
 const store = configureStore({
   reducer: {
