1a4e7d3f784e0cdc730947bf2ae78a2fffdb9b88
Leo5661 <singhra66@gmail.com>
2023-06-24 11:07:25 +0530

    Added: Details Screen

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,5 +1,17 @@
+import { useSelector } from "react-redux";
+
 function Details() {
-  return <div>Details</div>;
+  const name = useSelector((state) => state.student.studentName);
+  const age = useSelector((state) => state.student.studentAge);
+
+  return (
+    <div className="home flex h-full flex-col items-center justify-center bg-white">
+      <div className="title w-full text-center text-xl tracking-wider text-neutral-900">
+        {`Your name ${name} aged ${age} has been added to student system`}{" "}
+        <br /> You may exit
+      </div>
+    </div>
+  );
 }
 
 export default Details;
